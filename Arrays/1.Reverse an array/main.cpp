5ba6cd2df76c3f1997b33b9e9ff2d11759dcafa0
Vikash Raghuvanshi <54893386+vikashraghuvanshi@users.noreply.github.com>
2022-04-11 10:08:36 +0530

    Add files via upload

---
+++ Arrays/1.Reverse an array/main.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArr(int arr[], int start, int end){
+    while(start<end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArr(int arr[], int size)
+{
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+        cout<<endl;
+}
+
+
+
+int main()
+{
+    int arr[]  = {1,2,3,4,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printArr(arr,n);
+    reverseArr(arr,0,n-1);
+    cout<<"Reversed array is: "<<endl;
+    printArr(arr,n);
+    return 0;
+}
+
+
